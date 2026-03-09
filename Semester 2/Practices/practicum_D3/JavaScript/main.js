1eae55540e5ca0d5359ca26550b7bbe2151f5a2e
TheFusterMan <pleazzer073@gmail.com>
2026-03-09 15:54:40 +1000

    lab 2 step 2 done

--- Semester 2/Practices/practicum_D3/JavaScript/main.js
+++ Semester 2/Practices/practicum_D3/JavaScript/main.js
@@ -1,24 +1,24 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const width = 600;
-    const height = 600;
+    const width = 1280;
+    const height = 1280;
     const svg = d3.select("svg")
         .attr("width", width)
         .attr("height", height) ;
 
     const settingForm = document.getElementById("setting");
     const drawButton = settingForm.querySelector('input[value="Нарисовать"]');
+    const clearButton = settingForm.querySelector('input[value="Очистить"]');
 
     drawButton.addEventListener('click', () => draw(settingForm));
+    clearButton.addEventListener('click', () => clear(svg));
 });
 
 const draw = (dataForm) => {
     const svg = d3.select("svg")
     let pict = drawSmile(svg)
-    pict.attr("transform", `translate(${dataForm.cx.value},
-                                      ${dataForm.cy.value})`);
-    pict.attr("transform", `scale(${dataForm.sx.value},
-                                  ${dataForm.sy.value})`);
-    pict.attr("transform", `rotate(${dataForm.ang.value},
-                                  ${dataForm.cx.value},
-                                  ${dataForm.cy.value})`)
+    pict.attr("transform", `translate(${dataForm.cx.value}, ${dataForm.cy.value}) scale(${dataForm.sx.value}, ${dataForm.sy.value}) rotate(${dataForm.ang.value}, ${dataForm.cx.value}, ${dataForm.cy.value})`);
+}
+
+const clear = (svg) => {
+    svg.selectAll('*').remove();
 }
