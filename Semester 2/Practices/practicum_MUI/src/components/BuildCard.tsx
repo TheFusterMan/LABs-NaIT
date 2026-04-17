22a2dfed5247d78f547dbdf3c888c745079751e2
Dimpozix <pleazzer073@gmail.com>
2026-04-17 13:59:11 +1000

    17.04.2026 task 5 final steps

---
+++ Semester 2/Practices/practicum_MUI/src/components/BuildCard.tsx
@@ -0,0 +1,55 @@
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import { styled } from '@mui/material/styles';
+
+const StyledTypography = styled(Typography)(({ theme }) => ({
+    color: theme.palette.text.secondary,
+    textAlign: 'justify',
+    marginBottom: "10px"
+}))
+
+interface ComponentProps {
+    building: {
+        img: string,
+        title: string,
+        description: string[],
+    };
+    index: number;
+}
+
+function BuildCard({ building, index } : ComponentProps) {
+    return (
+        <Card sx={{
+            display: 'flex',
+            flexDirection: (index % 2) ? 'row-reverse' : 'row'
+        }}>
+            <Box>
+                <CardContent>
+                    <Typography gutterBottom variant="h5" >
+                        { building.title }
+                    </Typography>
+                    { building.description.map((item, ind) => (
+                        <StyledTypography key={ind} variant="body2">
+                            { item }
+                        </StyledTypography>
+                    ))}
+                </CardContent>
+                <CardActions sx={{ justifyContent: index % 2 ? 'end' : 'left'}} >
+                    <Button size="small">Подробнее</Button>
+                </CardActions>
+            </Box>
+            <CardMedia
+                component="img"
+                alt={ building.title }
+                image={ building.img }
+            />
+        </Card>
+    )
+}
+
+export default BuildCard;
